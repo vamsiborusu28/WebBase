c487edd52c5438ba6fd5f977c134d03f7be271aa
BORUSU LAKSHMANA SAI VAMSI <BO20422840@fvfhxbu4q6lr.hgu_lan>
2022-12-19 19:04:29 +0530

    Creating Full stack Javascript Repositery

---
+++ javascript/date.js
@@ -0,0 +1,32 @@
+const newdate=new Date();
+
+console.log(newdate);
+
+
+//conver the date into strings 
+console.log(newdate.toString());
+
+console.log(newdate.toISOString());
+
+//Date Functions
+
+console.log(newdate.getFullYear());
+
+console.log(newdate.getDay());
+
+console.log(newdate.getDate());
+
+console.log(newdate.getMonth());
+
+
+//date in milliseconds since the epoch (january  1 1970 midnight) 
+console.log(newdate.getTime());
+
+
+//date in hours minutes and seconds 
+console.log(newdate.getHours());
+
+console.log(newdate.getMinutes());
+console.log(newdate.getSeconds());
+
+console.log(newdate.getMilliseconds());
