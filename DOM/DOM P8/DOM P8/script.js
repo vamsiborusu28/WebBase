fc994e2b59cb46f7f542b4f338a8d063abc6aa71
vamsiborusu28 <vamsiborusu2000@gmail.com>
2023-02-12 22:17:48 +0530

    new dom project 8 is added

---
+++ DOM/DOM P8/DOM P8/script.js
@@ -0,0 +1,37 @@
+'use script';
+
+
+
+// First task
+const aside=document.querySelector('aside');
+
+
+aside.style.overflow='scroll';
+
+const h2=document.createElement('h2');
+h2.textContent='Vamsi Borusu ';
+h2.className='new-head';
+
+const para=document.createElement('p');
+para.textContent='Vamsi Borusu New Sensation in technology space launching revolutionising products from his company VBS';
+para.className='new-p';
+para.style.textAlign='left';
+
+
+const hr=document.createElement('hr');
+hr.className='hr-line';
+
+
+aside.appendChild(hr);
+aside.appendChild(h2);
+aside.appendChild(para);
+
+
+//Second task
+
+const body=document.querySelector('body');
+body.style.backgroundImage='none';
+
+
+//Third Task
+
