bf6860485d5ce5c5a8a88d2512b0b3f2ee8869cc
vamsiborusu28 <vamsiborusu2000@gmail.com>
2023-03-06 12:05:54 +0530

    changes are made in dom 7 js file

--- DOM/DOM P7/DOM P7/index.js
+++ DOM/DOM P7/DOM P7/index.js
@@ -1,3 +1,4 @@
+//Task1
 const anchorList=document.querySelectorAll('div>a');
 
 anchorList.forEach((element,index) => {
@@ -7,13 +8,14 @@ if(index%2){
 });
 
 
-
+//Task2
 const submit=document.querySelector('form>button');
-
+const text=document.querySelector('form>input');
 submit.addEventListener('click',(event)=>{
   
   anchorList.forEach((element,ind)=>{
     if(ind%2){
+    element.innerText=text.value;
     element.style.color='red';
     element.style.display='inline';
     }
