acd37746d5b2fefbbb4c8ccec5086fa4b6a64cf0
vamsiborusu28 <vamsiborusu2000@gmail.com>
2023-03-25 01:42:48 +0530

    new files are added in react-router

---
+++ React/React-Router/personal/src/Profile/Contact.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Contact(){
+
+    return(
+
+        <>
+        <section className="mx-[30%] my-[5%] px-[5%] py-[5%]">
+            <form className="border-2 rounded-xl flex flex-col justify-center items-center gap-[20px] text-xl py-[3%]">
+                <label>vamsiborusu@gmail.com</label>
+                <label>9876543210</label>
+                <label>https:/github.com/vamsiborusu28</label>
+            </form>
+        </section>
+        </>
+    )
+}
+
+export default Contact;
